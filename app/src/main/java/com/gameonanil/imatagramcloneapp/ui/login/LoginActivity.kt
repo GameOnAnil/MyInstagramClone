b93677a1d57cd90afb4be62cd2f045e23494b277
Anil Thapa <67099170+gameonanil@users.noreply.github.com>
2021-03-11 13:01:23 +0545

    Initial Commit

--- app/src/main/java/com/gameonanil/imatagramcloneapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gameonanil/imatagramcloneapp/ui/login/LoginActivity.kt
@@ -2,11 +2,26 @@ package com.gameonanil.imatagramcloneapp.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.gameonanil.imatagramcloneapp.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+         navController = navHostFragment.navController
+
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() ||super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/gameonanil/imatagramcloneapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/gameonanil/imatagramcloneapp/ui/main/MainActivity.kt
@@ -7,15 +7,11 @@ import androidx.navigation.fragment.NavHostFragment
 import com.gameonanil.imatagramcloneapp.R
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activit_main)
 
-        val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        navController.navigateUp()
     }
 }
