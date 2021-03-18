2132bf2ef0491ba407c2db2f5bed045a2d73d953
Anil Thapa <67099170+gameonanil@users.noreply.github.com>
2021-03-18 16:18:24 +0545

    Minor layout change

--- app/src/main/java/com/gameonanil/instagramcloneapp/ui/start/StartActivity.kt
+++ app/src/main/java/com/gameonanil/instagramcloneapp/ui/start/StartActivity.kt
@@ -3,6 +3,9 @@ package com.gameonanil.instagramcloneapp.ui.start
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -18,6 +21,7 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
 
         val navHostFragment =supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
